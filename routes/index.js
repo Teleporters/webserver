69406163dae2cb7ebe2af19d07a502fd70b3de6d
Martin Naumann <martin@geekonaut.de>
2015-05-26 21:44:12 +0200

    Changing the code to use the public file name, rather than the hidden ID.

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var mongo = require('mongojs');
 var db = mongo(process.env.MONGOLAB_URI, ['teleports']);
 
 /* GET home page. */
-router.get('/:id', function(req, res, next) {
-  db.teleports.findOne({_id: mongo.ObjectId(req.params.id)}, function(err, spot) {
+router.get('/:file', function(req, res, next) {
+  db.teleports.findOne({file: req.params.file}, function(err, spot) {
     if(err) {
       res.render('error', {message: 'Unknown location'})
       return
@@ -16,7 +16,8 @@ router.get('/:id', function(req, res, next) {
       title: spot.title,
       id: req.params.id,
       imageUrl: 'http://teleports.s3-website-eu-west-1.amazonaws.com/portals/' + spot.file + '.jpg',
-      previewUrl: 'http://teleports.s3-website-eu-west-1.amazonaws.com/thumbs/' + spot.file + '.png'      
+      previewUrl: 'http://teleports.s3-website-eu-west-1.amazonaws.com/thumbs/' + spot.file + '.png',
+      file: spot.file
     });
   })
 });
